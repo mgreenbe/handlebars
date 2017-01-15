c81c54f6a2ba87eb796477660d368606dfe4cf4d
fmgreenberg <fmgreenberg@gmail.com>
2017-01-15 09:40:08 -0700

    remove buttons working; wierd nulls in model array fields

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const templatePlaceholder = `{{#each people}}
     <p><label>Name:</label><input class="form-control" value="{{name}}" data-path="people[{{@index}}].name"></p>
     <label>Kids:</label>
     {{#each kids}}
+      <div class="input-group">
         <input class="form-control" value="{{this}}" data-path="people[{{@../index}}].kids[{{@index}}]">
-        {{/each}}
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button" data-action="REMOVE" data-path="people[{{@../index}}].kids[{{@index}}]">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+        </span>
+      </div>
+    {{/each}}
     <button class="btn btn-default" data-action="ADD" data-path="people[{{@index}}].kids">Add kid</button>
     <br><br>
 {{/each}}
@@ -40,6 +47,9 @@ const compileButton = document.getElementById("compile-button");
 const preview = document.getElementById("preview");
 preview.template = "TeMpLaTe";
 
+const ancestor = (elt, stop, test) => test(elt) ? elt :
+  (elt == stop) ? null : ancestor(elt.parentElement, stop, test);
+
 let context;
 
 compileButton.addEventListener('click', () => {
@@ -49,13 +59,6 @@ compileButton.addEventListener('click', () => {
   preview.innerHTML = html;
 });
 
-// const handleClick = function(e) {
-//   context.people.push("Matthew Greenberg");
-//   const template = Handlebars.compile(templateEditor.getValue());
-//   const html = template(context);
-//   preview.innerHTML = html;
-// }
-
 const handleInput = function(e) {
   _.set(context, e.target.dataset.path, e.target.value);
   console.log(`context: ${JSON.stringify(context)}`);
@@ -64,13 +67,30 @@ const handleInput = function(e) {
 preview.addEventListener("input", handleInput);
 
 const handleClick = function(e) {
-  if (e.target.hasAttribute("data-path") && e.target.hasAttribute("data-action")) {
-    const path = e.target.dataset.path;
-    let array = _.get(context, path);
-    if (!Array.isArray(array)) {
-      array = _.get(_.set(context, path, []), path);
+  console.log(e.target);
+  const target = ancestor(e.target, preview, (elt) => 
+    (elt.hasAttribute("data-path") && elt.hasAttribute("data-action"))
+  );
+  console.log(target);
+  if (target) {
+    const action = target.dataset.action;
+    const path = target.dataset.path;
+    console.log(`action: ${action}, path: ${path}`);
+    switch (action) {
+      case "ADD":
+        let array = _.get(context, path);
+        if (!Array.isArray(array)) {
+          array = _.get(_.set(context, path, []), path);
+        }
+        array.push(null);
+        break;
+      case "REMOVE":
+        console.log("case remove");
+        _.unset(context, path);
+        break;
+      default:
+        console.log("default");
     }
-    array.push(null);
     const template = Handlebars.compile(templateEditor.getValue());
     const html = template(context);
     preview.innerHTML = html;
@@ -78,3 +98,6 @@ const handleClick = function(e) {
 }
 
 preview.addEventListener("click", handleClick);
+
+contextButton = document.getElementById("get-context");
+contextButton.addEventListener("click", () => console.log(JSON.stringify(context, null, 2)));
